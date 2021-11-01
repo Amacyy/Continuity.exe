b2a8956377bd0f11824e8f47567c5e436ee503f0
MacTheMink <90684015+MacTheMink@users.noreply.github.com>
2021-11-01 14:27:02 +0000

    Score code added

--- Game Dev Group Project/Assets/code/Car.cs
+++ Game Dev Group Project/Assets/code/Car.cs
@@ -8,28 +8,12 @@ public class Car : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
-        Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
-        Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
-
-        minY = bottomCorner.y;
-        maxY = topCorner.y;
-
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get current position
-        Vector3 pos = transform.position;
-
-
-        // vertical contraint
-        if (pos.y < minY) pos.y = minY;
-        if (pos.y > maxY) pos.y = maxY;
-
-        // Update position
-        transform.position = pos;
 
         MoveCharacter();
 
@@ -41,6 +25,10 @@ public class Car : MonoBehaviour
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.DownArrow))
             transform.Translate(Vector3.down* speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 
     float speed = 10.0f;
