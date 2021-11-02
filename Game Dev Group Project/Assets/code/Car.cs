7b5c532614d7afb8af8eb4f0d8b77e2db02096c3
MacTheMink <90684015+MacTheMink@users.noreply.github.com>
2021-11-02 10:20:38 +0000

    movement code fixed

--- Game Dev Group Project/Assets/code/Car.cs
+++ Game Dev Group Project/Assets/code/Car.cs
@@ -21,13 +21,13 @@ public class Car : MonoBehaviour
 
     private void MoveCharacter()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(Vector3.down* speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.down* speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
             transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 
